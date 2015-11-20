22ba08ee3d9aca8ca38433e5eb3ca45d5c989f2b
Heike Hofmann <hofmann@iastate.edu>
2015-11-20 14:35:10 -0600

    start and end of lines defined

--- code/matching.R
+++ code/matching.R
@@ -40,7 +40,7 @@ lof <- processBullets(paths = images[c(5,7)], x = 100)
 
 subLOFx1 <- subset(lof, bullet=="Br1 Bullet 1-5")
 subLOFx2 <- subset(lof, bullet=="Br1 Bullet 2-1")
-subLOFx1$y <- subLOFx1$y + 19*1.5625 # working now!!!
+subLOFx1$y <- subLOFx1$y + 20*1.5625 # working now!!!
 lof <- rbind(data.frame(subLOFx1), data.frame(subLOFx2))
 
 
@@ -57,6 +57,8 @@ qplot(y, resid, group=bullet, colour=bullet, data=lof, geom="line") +
 lof <- lof %>% group_by(bullet) %>% mutate(
   l30 = smoothloess(y, resid, span = 0.03)
 )
+lof <- subset(lof, !is.na(l30))
+
 lof$r05 <- 0.75* sign(lof$l30) * as.numeric(abs(lof$l30) > .75)
 lof$type <- factor(lof$r05)
 levels(lof$type) <- c("groove", NA, "peak")
@@ -71,4 +73,33 @@ qplot(data=lof, x=y, y=bullet, fill=type, geom="tile") +
   ylab("") +
   theme(legend.position="bottom")
 
+lof <- lof %>% group_by(bullet) %>% mutate(
+  diff = c(0, diff(r05)),
+  mins = list(c(1, which(diff != 0))),
+  maxs = list(c(which(diff != 0)-1, n())),
+  n = n(),
+  minMarks = list(mins[[1]][!is.na(type[mins[[1]]])]),
+  maxMarks = list(maxs[[1]][!is.na(type[maxs[[1]]])])
+)
+
+marks <- lof %>% group_by(bullet) %>% summarise(
+  mins = mins[1],
+  maxs = maxs[1],
+  n = n[1],
+  minMarks = minMarks[1],
+  maxMarks = maxMarks[1]
+)
+
+mins <- marks$minMarks[1][[1]]
+maxs <- marks$maxMarks[1][[1]]
+for (i in 2:nrow(marks)) {
+  mins <- c(mins, marks$n[i-1] + marks$minMarks[i][[1]])
+  maxs <- c(maxs, marks$n[i-1] + marks$maxMarks[i][[1]])
+}
+
+# start and end of the marks
+qplot(data=lof, x=y, y=l30, colour=type, geom="line", group=bullet) + 
+  facet_grid(bullet~.) +
+  geom_point(aes(y=r05), data=lof[mins,]) +
+  geom_point(aes(y=r05), data=lof[maxs,])
 
