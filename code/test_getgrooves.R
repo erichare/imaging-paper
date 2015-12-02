b94f9964cbbd5b41a367ca3b2e89e1423680a0bf
Eric Hare <ericrhare@gmail.com>
2015-12-02 08:53:35 -0600

    Update testing code for grooves

--- code/test_getgrooves.R
+++ code/test_getgrooves.R
@@ -2,9 +2,8 @@ library(x3pr)
 library(x3prplus)
 library(ggplot2)
 
-setwd("~/Downloads/Hamby252_3DX3P1of2/")
-for (file in dir()) {
-    x <- get_grooves(get_bullet(file), smoothfactor = 41)
+for (file in dir("app/images/Hamby252_3DX3P1of2")) {
+    x <- get_grooves(get_bullet(file.path("app/images/Hamby252_3DX3P1of2", file), x = 100), smoothfactor = 41)
     cat(file, "\n")
     print(x$plot)
     value <- readline("Press Enter to Continue, or q to Quit")
