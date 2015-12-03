c83049f010573c568cc07fe6b51cd25bb0288cac
Eric Hare <ericrhare@gmail.com>
2015-12-02 20:25:52 -0600

    Small change to testgrooves

--- code/test_getgrooves.R
+++ code/test_getgrooves.R
@@ -3,7 +3,7 @@ library(x3prplus)
 library(ggplot2)
 
 for (file in dir("app/images/Hamby252_3DX3P1of2")) {
-    mybullet <- get_bullet(file.path("app/images/Hamby252_3DX3P1of2", file), x = 150)
+    mybullet <- get_bullet(file.path("app/images/Hamby252_3DX3P1of2", file), x = 100)
     x <- get_grooves(mybullet)
     cat(file, "\n")
     print(x$plot)
@@ -12,6 +12,3 @@ for (file in dir("app/images/Hamby252_3DX3P1of2")) {
     value <- readline("Press Enter to Continue, or q to Quit")
     if (value == "q") break;
 }
-
-mybullet <- get_bullet(file.path("app/images/Hamby252_3DX3P1of2", "Br1 Bullet 1-5.x3p"), x = 100)
-x <- get_grooves(mybullet)
