eb0c4048f07bcd4c6d8e40540a8b6feb92602786
Eric Hare <ericrhare@gmail.com>
2016-04-03 13:30:06 -0500

    More groove detection work

--- code/test_getgrooves.R
+++ code/test_getgrooves.R
@@ -2,26 +2,27 @@ library(x3pr)
 library(x3prplus)
 library(ggplot2)
 
-path1 <- "~/Downloads/Hamby Set 44/known/"
-for (file in dir(path1)) {
-    mybullet <- get_crosscut(file.path(path1, file), x = 100, transpose = TRUE)
-    x <- get_grooves(mybullet, groove_cutoff = 500)
+crosscuts_44 <- read.csv("csvs/crosscuts-25-set44.csv")
+apply(crosscuts_44, 1, function(row) {
+    cat(row[1])
+    mybullet <- get_crosscut(row[1], x = as.numeric(row[2]), transpose = TRUE)
+    x <- get_grooves(mybullet, smoothfactor = 35, groove_cutoff = 500)
     cat(file, "\n")
     print(x$plot)
     #my.loess <- fit_loess(mybullet, x)
     #print(my.loess$resid)
     value <- readline("Press Enter to Continue, or q to Quit")
     if (value == "q") break;
-}
+})
 
-path2 <- "~/GitHub/imaging-paper/app/images/Hamby252_3DX3P1of2/"
-for (file in dir(path2)) {
-    mybullet <- get_crosscut(file.path(path2, file), x = 100)
+crosscuts <- read.csv("csvs/crosscuts-25.csv")
+apply(crosscuts, 1, function(row) {
+    cat(row[1])
+    mybullet <- get_crosscut(row[1], x = as.numeric(row[2]))
     x <- get_grooves(mybullet, smoothfactor = 15)
-    cat(file, "\n")
     print(x$plot)
     #my.loess <- fit_loess(mybullet, x)
     #print(my.loess$resid)
     value <- readline("Press Enter to Continue, or q to Quit")
-    if (value == "q") break;
-}
+    if (value == "q") break;    
+})
