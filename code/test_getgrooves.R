6c689e67f8c37855cb9e835a2f5e2c78f1b9a2fc
Eric Hare <ericrhare@gmail.com>
2016-04-03 07:54:02 -0500

    Groove testing code

--- code/test_getgrooves.R
+++ code/test_getgrooves.R
@@ -2,9 +2,22 @@ library(x3pr)
 library(x3prplus)
 library(ggplot2)
 
-for (file in dir("~/Downloads/Hamby Set 44/known/")) {
-    mybullet <- get_crosscut(file.path("~/Downloads/Hamby Set 44/known/", file), x = 100, transpose = TRUE)
-    x <- get_grooves(mybullet)
+path1 <- "~/Downloads/Hamby Set 44/known/"
+for (file in dir(path1)) {
+    mybullet <- get_crosscut(file.path(path1, file), x = 100, transpose = TRUE)
+    x <- get_grooves(mybullet, groove_cutoff = 500)
+    cat(file, "\n")
+    print(x$plot)
+    #my.loess <- fit_loess(mybullet, x)
+    #print(my.loess$resid)
+    value <- readline("Press Enter to Continue, or q to Quit")
+    if (value == "q") break;
+}
+
+path2 <- "~/GitHub/imaging-paper/app/images/Hamby252_3DX3P1of2/"
+for (file in dir(path2)) {
+    mybullet <- get_crosscut(file.path(path2, file), x = 100)
+    x <- get_grooves(mybullet, smoothfactor = 15)
     cat(file, "\n")
     print(x$plot)
     #my.loess <- fit_loess(mybullet, x)
