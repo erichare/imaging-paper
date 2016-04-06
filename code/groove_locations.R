7a5a7843f603e7506b46cc129a6b3fe7aecd1678
Eric Hare <ericrhare@gmail.com>
2016-04-06 11:52:33 -0500

    Mean grooves, and working on all grooves

--- code/groove_locations.R
+++ code/groove_locations.R
@@ -10,7 +10,7 @@ knowns <- file.path(knowndatadir, dir(knowndatadir, pattern="x3p"))
 unknowndatadir <- "app/images/Hamby252_3DX3P2of2/"
 unknowns <- file.path(unknowndatadir, dir(unknowndatadir))
 
-crosscuts <- lapply(c(knowns, unknowns), function(bul) {
+groove_locations <- lapply(c(knowns, unknowns), function(bul) {
     cat(bul, "\n")
     bullet <- read.x3pplus(bul)
     fortified <- fortify_x3p(bullet)
@@ -27,3 +27,21 @@ crosscuts <- lapply(c(knowns, unknowns), function(bul) {
     
     return(result)
 })
+
+groove_locations_means <- lapply(c(knowns, unknowns), function(bul) {
+    cat(bul, "\n")
+    bullet <- read.x3pplus(bul)
+    fortified <- fortify_x3p(bullet)
+    
+    fort.mean <- fortified %>%
+        group_by(y) %>%
+        summarise(value = mean(value, na.rm = TRUE))
+    
+    cc <- cbind(x = NA, fort.mean)
+    result <- get_grooves(cc, smoothfactor = 15)
+
+    return(result$groove)
+})
+groove.means <- cbind(data.frame(bullet = c(knowns, unknowns), do.call(rbind, groove_locations_means)))
+names(groove.means) <- c("bullet", "groove_left", "groove_right")
+write.csv(groove.means, file = "csvs/grooves-mean.csv", row.names = FALSE)
