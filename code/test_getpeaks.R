656225b01e6299e921e1b8c01c31c3a7d71ec334
Eric Hare <ericrhare@gmail.com>
2016-07-11 16:07:01 -0400

    Some small typos

--- code/test_getpeaks.R
+++ code/test_getpeaks.R
@@ -3,11 +3,11 @@ library(x3prplus)
 library(ggplot2)
 
 for (file in dir("app/images/Hamby252_3DX3P1of2")) {
-    mybullet <- get_bullet(file.path("app/images/Hamby252_3DX3P1of2", file), x = 150)
+    mybullet <- get_crosscut(file.path("app/images/Hamby252_3DX3P1of2", file), x = 150)
     x <- get_grooves(mybullet)
     cat(file, "\n")
     my.loess <- fit_loess(mybullet, x)
-    result <- get_peaks(my.loess$data)
+    result <- get_peaks(my.loess$data, smoothfactor = 15)
     print(result$plot)
     value <- readline("Press Enter to Continue, or q to Quit")
     if (value == "q") break;
