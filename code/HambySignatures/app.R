f66b9c979aca8dc28552c0b9b2a773a8da7c1a50
Eric Hare <ericrhare@gmail.com>
2016-10-02 09:18:27 -0500

    Get app ready but kind of stuck

--- code/HambySignatures/app.R
+++ code/HambySignatures/app.R
@@ -2,6 +2,7 @@ library(shiny)
 
 crosscuts <- read.csv("~/GitHub/imaging-paper/csvs/crosscuts.csv")
 grooves <- read.csv("~/GitHub/imaging-paper/csvs/grooves.csv")
+bullet_stats <- read.csv("~/GitHub/imaging-paper/data-25-25/bullet-stats.csv")
 
 ui <- fluidPage(
    
