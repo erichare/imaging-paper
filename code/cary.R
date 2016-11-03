151c8169a19a4fffc4979722b18af56d505d0311
Eric Hare <ericrhare@gmail.com>
2016-11-03 18:21:31 -0500

    Finish up cary study

--- code/cary.R
+++ code/cary.R
@@ -2,6 +2,7 @@ library(x3pr)
 library(x3prplus)
 library(ggplot2)
 library(dplyr)
+library(randomForest)
 
 load("~/GitHub/x3prplus/data/rf.RData")
 
@@ -19,7 +20,7 @@ cary1_groove$plot
 
 cary1_loess <- fit_loess(bullet = cary1_cross, groove = list(groove = cary1_groove$groove), span = .03)
 cary1_processed <- processBullets(bullet = cary1, name = "cary1", x = myx, grooves = cary1_groove$groove)
-
+cary1_smoothed <- bulletSmooth(cary1_processed)
 
 other_carys <- lapply(cary_paths[-1], function(x) {
     result <- read.x3pplus(x)
@@ -41,4 +42,73 @@ carys_processed <- lapply(other_carys, function(bul) {
     processBullets(bullet = bul, name = bul$path, x = myx, grooves = cary_groove$groove)
 })
 names(carys_processed) <- cary_paths[-1]
+cary_smoothed <- carys_processed %>% bind_rows %>% bulletSmooth
+
+if (!file.exists("cary-data")) dir.create("cary-data")
+reslist <- lapply(other_carys, function(x) {
+    cat("Processing cary1 vs", basename(x$path), "\n")
+    
+    br2 <- filter(cary_smoothed, bullet == x$path)
+    bulletGetMaxCMS(cary1_smoothed, br2, column = "l30", span = 25)
+})
+
+ccf <- plyr::ldply(reslist, function(res) {
+    if (is.null(res)) return(NULL)
+    lofX <- res$bullets
+    b12 <- unique(lofX$bullet)
+    
+    subLOFx1 <- subset(lofX, bullet==b12[1])
+    subLOFx2 <- subset(lofX, bullet==b12[2]) 
+    
+    ys <- intersect(subLOFx1$y, subLOFx2$y)
+    idx1 <- which(subLOFx1$y %in% ys)
+    idx2 <- which(subLOFx2$y %in% ys)
+    distr.dist <- mean((subLOFx1$val[idx1] - subLOFx2$val[idx2])^2, na.rm=TRUE)
+    distr.sd <- sd(subLOFx1$val, na.rm=TRUE) + sd(subLOFx2$val, na.rm=TRUE)
+    km <- which(res$lines$match)
+    knm <- which(!res$lines$match)
+    if (length(km) == 0) km <- c(length(knm)+1,0)
+    if (length(knm) == 0) knm <- c(length(km)+1,0)
+    #browser()    
+    # feature extraction
+    signature.length <- min(nrow(subLOFx1), nrow(subLOFx2))
+    
+    data.frame(ccf=res$ccf, lag=res$lag, 
+               D=distr.dist, 
+               sd.D = distr.sd,
+               b1=b12[1], b2=b12[2], x1 = subLOFx1$x[1], x2 = subLOFx2$x[1],
+               signature.length = signature.length,
+               #num.matches = sum(res$lines$match),
+               matches.per.y = sum(res$lines$match) / signature.length,
+               #num.mismatches = sum(!res$lines$match), 
+               mismatches.per.y = sum(!res$lines$match) / signature.length,
+               #cms = res$maxCMS,
+               cms.per.y = res$maxCMS / signature.length,
+               #cms2 = x3prplus::maxCMS(subset(res$lines, type==1 | is.na(type))$match),
+               cms2.per.y = x3prplus::maxCMS(subset(res$lines, type==1 | is.na(type))$match) / signature.length,
+               #non_cms = x3prplus::maxCMS(!res$lines$match),
+               non_cms.per.y = x3prplus::maxCMS(!res$lines$match) / signature.length,
+               #left_cms = max(knm[1] - km[1], 0),
+               left_cms.per.y = max(knm[1] - km[1], 0) / signature.length,
+               #right_cms = max(km[length(km)] - knm[length(knm)],0),
+               right_cms.per.y = max(km[length(km)] - knm[length(knm)],0) / signature.length,
+               #left_noncms = max(km[1] - knm[1], 0),
+               left_noncms.per.y = max(km[1] - knm[1], 0) / signature.length,
+               #right_noncms = max(knm[length(knm)]-km[length(km)],0),
+               right_noncms.per.y = max(knm[length(knm)]-km[length(km)],0) / signature.length,
+               #sumpeaks = sum(abs(res$lines$heights[res$lines$match]))
+               sumpeaks.per.y = sum(abs(res$lines$heights[res$lines$match])) / signature.length
+    )
+})
+write.csv(ccf, file = "cary-stats.csv", row.names = FALSE)
+myprobs <- predict(rtrees, ccf, type = "prob")
+
+iteration <- as.numeric(gsub("../images/Cary Persistence/bullets/CWBLT([0-9]+)-1.x3p", "\\1", cary_paths))
+qplot(iteration[-1], myprobs[,2], geom = "point") + 
+    theme_bw() + 
+    geom_line() +
+    xlab("Cary Persistence Iteration") +
+    ylab("Predicted Probability of Match ") +
+    geom_hline(yintercept = 0.5) +
+    ggtitle("Predicted Prob of Match (Cary 1 vs Cary 2-2000)")
 
