1b885a327f902d763835a2d44f237f77ba7b1ecc
Eric Hare <ericrhare@gmail.com>
2016-11-30 12:58:45 -0500

    Fix paper building on linux

--- code/test_readx3p.R
+++ code/test_readx3p.R
@@ -6,7 +6,7 @@ datamat <- mybullet$surface.matrix
 
 plot_ly(z = ~datamat) %>% add_surface()
 
-mybullet2 <- read_x3p("images/Hamby (2009) Barrel/bullets/br1_1_land1.x3p", transpose = TRUE)
+mybullet2 <- read_x3p("images/Hamby (2009) Barrel/bullets/br1_1_land1.x3p")
 datamat2 <- mybullet2$surface.matrix
 
 plot_ly(z = ~datamat2) %>% add_surface()
